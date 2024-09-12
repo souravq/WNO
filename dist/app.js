a94696d0dc98c1d341bbb54f23f466fb18d03652
souravq <souravbera515@gmail.com>
2024-09-12 20:43:29 +0530

    dist file changes

--- dist/app.js
+++ dist/app.js
@@ -6,16 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
-const news_router_1 = __importDefault(require("../src/app/modules/news/news.router"));
+const news_router_1 = __importDefault(require("./app/modules/news/news.router"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
-app.use("/", (req, res) => {
-    res.send("Hello World");
+app.use('/', (req, res) => {
+    res.send('Hello World');
 });
 // News Management Routes
 app.use('/api/v1', news_router_1.default);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Export the app instead of starting the server
+exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,19 @@
 import express from 'express';
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser';
 import cors from 'cors';
-import newsRoutes from "../src/app/modules/news/news.router";
+import newsRoutes from './app/modules/news/news.router';
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/",(req,res)=>{
-  res.send("Hello World");
-})
+app.use('/', (req, res) => {
+  res.send('Hello World');
+});
 
 // News Management Routes
 app.use('/api/v1', newsRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+// Export the app instead of starting the server
+export default app;
