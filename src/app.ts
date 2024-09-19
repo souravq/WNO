254d344a4e88f2f79fe96128eaebe535a4d5c155
souravq <souravbera515@gmail.com>
2024-09-19 19:49:24 +0530

    all origin allowed

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,41 @@
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// import cors from 'cors';
+// import newsRoutes from './app/modules/news/news.router';
+
+// const app = express();
+
+
+// // Configure CORS
+// const corsOptions = {
+//   origin: [
+//       'https://staging.d33iveo2ig7ous.amplifyapp.com',
+//       'https://staging.d1o23w9vuaoufu.amplifyapp.com'
+//   ],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// };
+
+// app.use(cors(corsOptions));
+
+// app.options('*', cors(corsOptions));  // Handle preflight requests
+
+// app.use(bodyParser.json());
+
+// // app.use('/', (req, res) => {
+// //   res.send('Hello World');
+// // });
+
+// // News Management Routes
+// app.use('/api/v1', newsRoutes);
+
+// app.listen(5000, () => {
+//   console.log('Server started on port 5000');
+// });
+
+// // Export the app instead of starting the server
+// export default app;
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
@@ -5,26 +43,17 @@ import newsRoutes from './app/modules/news/news.router';
 
 const app = express();
 
-
-// Configure CORS
+// Configure CORS to allow all origins
 const corsOptions = {
-  origin: [
-      'https://staging.d33iveo2ig7ous.amplifyapp.com',
-      'https://staging.d1o23w9vuaoufu.amplifyapp.com'
-  ],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
-
 app.options('*', cors(corsOptions));  // Handle preflight requests
 
 app.use(bodyParser.json());
-
-// app.use('/', (req, res) => {
-//   res.send('Hello World');
-// });
 
 // News Management Routes
 app.use('/api/v1', newsRoutes);
