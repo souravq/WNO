cde83363714788a596408798c4e346a2b4c23fbb
souravq <souravbera515@gmail.com>
2024-09-19 19:08:17 +0530

    cors policy updated

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,27 @@ import newsRoutes from './app/modules/news/news.router';
 
 const app = express();
 
-app.use(cors());
+
+// Define the allowed origins
+const allowedOrigins = [
+  'https://staging.d33iveo2ig7ous.amplifyapp.com', 
+  'https://staging.d1o23w9vuaoufu.amplifyapp.com'
+];
+
+const corsOptions = {
+  origin: (origin:any, callback:any) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      // Allow the request if the origin is in the allowed list or it's a server-to-server request with no origin
+      callback(null, true);
+    } else {
+      // Deny the request if the origin is not allowed
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // If you want to allow cookies or other credentials
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // app.use('/', (req, res) => {
